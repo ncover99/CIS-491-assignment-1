1f45f2238482a45eab728ae6e8a23b151918e5e1
ncover99 <56737302+ncover99@users.noreply.github.com>
2021-01-28 10:38:53 -0600

    basic movement and sprites

---
+++ CIS497_assignment_01_ncover/Assets/Scripts/Firearm.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Firearm : MonoBehaviour
+{
+    [SerializeField] protected int ammoPerMag = 11;
+    [SerializeField] protected float roteOfFire = 0.2f;
+    [SerializeField] protected GameObject bullet;
+    
+    public abstract void Fire();
+
+    public abstract void Reload();
+    
+    
+}
---
+++ CIS497_assignment_01_ncover/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Assignment_01
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float speed = 5f;
+        [SerializeField]private float xMove = 0f;
+        [SerializeField]private float yMove = 0f;
+
+        private Rigidbody2D rb2d;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            xMove = Input.GetAxis("Horizontal");
+            yMove = Input.GetAxis("Vertical");
+            
+            
+            // rotate to face cursor
+            var screenPos = Camera.main.WorldToScreenPoint(transform.position); // player position in camera space
+            var mouseDir = Input.mousePosition - screenPos;
+            var angle = Mathf.Atan2(mouseDir.y, mouseDir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle + -90, Vector3.forward);
+            
+            // just some stuff to help me with directions
+            Debug.DrawRay(transform.position, Vector3.forward * 1, Color.blue);
+            Debug.DrawRay(transform.position, Vector3.up * 1, Color.green);
+            Debug.DrawRay(transform.position, Vector3.right * 1, Color.red);
+
+        }
+
+        void FixedUpdate()
+        {
+            // movement
+            rb2d.MovePosition(rb2d.position + (new Vector2(xMove, yMove) * speed) * Time.fixedDeltaTime);
+        }
+    }   
+}
